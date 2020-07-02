32f2c140a3a50284f971812efacae511e9b48203
pixelzoom <cmalley@pixelzoom.com>
2020-07-02 14:59:47 -0600

    update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
+
+/* eslint-env node */
 
 // use chipper's gruntfile
 module.exports = require( '../chipper/js/grunt/Gruntfile.js' );
