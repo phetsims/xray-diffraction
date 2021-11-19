f58804495fb36e3d4d6f0d1cfa25d46e5ee2dc9a
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:44 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/xrayDiffractionStrings.ts
@@ -0,0 +1,39 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import xrayDiffraction from './xrayDiffraction.js';
+
+type StringsType = {
+  'xray-diffraction': {
+    'title': string
+  },
+  'angleUnit': string,
+  'aLatticeConstant': string,
+  'bLatticeConstant': string,
+  'aLatticeEquals': string,
+  'bdLatticeEquals': string,
+  'braggEquation': string,
+  'dSinTheta': string,
+  'horizontalRays': string,
+  'incidentAngle': string,
+  'inPhase': string,
+  'interplaneDistance': string,
+  'lengthUnit': string,
+  'moreParameters': string,
+  'pathDifference': string,
+  'pLD': string,
+  'showTransmitted': string,
+  'verticalRays': string,
+  'wavelength': string,
+  'waveFrontMarkers': string
+};
+
+const xrayDiffractionStrings = getStringModule( 'XRAY_DIFFRACTION' ) as StringsType;
+
+xrayDiffraction.register( 'xrayDiffractionStrings', xrayDiffractionStrings );
+
+export default xrayDiffractionStrings;
