1d25f3581f29e985c0c63573e19186b213d80fba
phet-dev <phetadmin@gmail.com>
2022-03-05 03:30:44 -0700

    update copyright dates from daily grunt work

--- js/xray-diffraction/view/XrayControlPanel.js
+++ js/xray-diffraction/view/XrayControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Shows the main controls, including frequency/wavelength and amplitude.
