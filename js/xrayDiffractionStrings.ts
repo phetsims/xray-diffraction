d5fa7799d30619c1f1b77ba9fd3a2e3aea9dde19
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:38 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/xrayDiffractionStrings.ts
+++ js/xrayDiffractionStrings.ts
@@ -5,31 +5,52 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import xrayDiffraction from './xrayDiffraction.js';
 
 type StringsType = {
   'xray-diffraction': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'angleUnit': string;
+  'angleUnitProperty': TReadOnlyProperty<string>;
   'aLatticeConstant': string;
+  'aLatticeConstantProperty': TReadOnlyProperty<string>;
   'bLatticeConstant': string;
+  'bLatticeConstantProperty': TReadOnlyProperty<string>;
   'aLatticeEquals': string;
+  'aLatticeEqualsProperty': TReadOnlyProperty<string>;
   'bdLatticeEquals': string;
+  'bdLatticeEqualsProperty': TReadOnlyProperty<string>;
   'braggEquation': string;
+  'braggEquationProperty': TReadOnlyProperty<string>;
   'dSinTheta': string;
+  'dSinThetaProperty': TReadOnlyProperty<string>;
   'horizontalRays': string;
+  'horizontalRaysProperty': TReadOnlyProperty<string>;
   'incidentAngle': string;
+  'incidentAngleProperty': TReadOnlyProperty<string>;
   'inPhase': string;
+  'inPhaseProperty': TReadOnlyProperty<string>;
   'interplaneDistance': string;
+  'interplaneDistanceProperty': TReadOnlyProperty<string>;
   'lengthUnit': string;
+  'lengthUnitProperty': TReadOnlyProperty<string>;
   'moreParameters': string;
+  'moreParametersProperty': TReadOnlyProperty<string>;
   'pathDifference': string;
+  'pathDifferenceProperty': TReadOnlyProperty<string>;
   'pLD': string;
+  'pLDProperty': TReadOnlyProperty<string>;
   'showTransmitted': string;
+  'showTransmittedProperty': TReadOnlyProperty<string>;
   'verticalRays': string;
+  'verticalRaysProperty': TReadOnlyProperty<string>;
   'wavelength': string;
+  'wavelengthProperty': TReadOnlyProperty<string>;
   'waveFrontMarkers': string;
+  'waveFrontMarkersProperty': TReadOnlyProperty<string>;
 };
 
 const xrayDiffractionStrings = getStringModule( 'XRAY_DIFFRACTION' ) as StringsType;
